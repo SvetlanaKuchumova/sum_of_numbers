f71fe37686421123545289f32b4c146dd768dc96
svetlanaand <svetikpodv@yandex.ru>
2019-07-06 23:59:20 +0500

    1

--- src/main.java
+++ src/main.java
@@ -1,25 +1,67 @@
-import javax.swing.*;
-//import java.util.Scanner;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.Scanner;
+
 
 public class main {
 
-    public static void main(String[] args)
-    {
-       // Simple
+    public static void main(String[] args) {
+        System.out.println("Введите цифру:\n 1 - запуск окна с текстом, \n 2 - запуск окна с расчетом суммы \n Enter - выход ");
+        Scanner scan = new Scanner(System.in);
 
-     //   frame.setVisible(true);
-        /*Scanner scan = new Scanner(System.in);
-        //System.out.println("Введи свое имя");
-        //String s1 = scan.nextLine();
-        //System.out.println("Привет, " + s1 + "! Эта программа считает сумму двух целых чисел");
-        System.out.print("Введи первое число: ");
-        int number1 = scan.nextInt();
-        System.out.print("Введи второе число: ");
-        int number2 = scan.nextInt();
-        int sum = number1+number2;
-        System.out.println ("Сумма чисел равна " + sum);*/
+        String str = scan.nextLine();
+        gui_text app1 = new gui_text();
+        calc app2 = new calc();
+
+        while (!str.isEmpty()) {
+            try {
+                int number = Integer.parseInt(str);
+                if (number == 1 || number == 2) {
+                    if (number == 1) {
+                        if (app2.isVisible()) {
+                            app2.setVisible(false);
+                            app2.dispose();
+                        }
+                        app1.setVisible(true);
+                        System.out.println("Введите цифру:\n 1 - запуск окна с текстом, \n 2 - запуск окна с расчетом суммы \n Enter - выход ");
+                    } else { // number == 2
+                        if (app1.isVisible()) {
+                            app1.setVisible(false);
+                            app1.dispose();
+                        }
+                        app2.setVisible(true);
+                        System.out.println("Введите цифру:\n 1 - запуск окна с текстом, \n 2 - запуск окна с расчетом суммы \n Enter - выход ");
+                    }
+                } else {
+                    if (app1.isVisible()) {
+                        app1.setVisible(false);
+                        app1.dispose();
+                    }
+                    if (app2.isVisible()) {
+                        app2.setVisible(false);
+                        app2.dispose();
+                    }
+                    System.out.println("Введите цифру 1 или 2");
+                }
+            } catch (NumberFormatException exp) {
+                if (app1.isVisible()) {
+                    app1.setVisible(false);
+                    app1.dispose();
+                }
+                if (app2.isVisible()) {
+                    app2.setVisible(false);
+                    app2.dispose();
+                }
+                System.out.println("Некорректный ввод. Введите цифру 1 или 2");
+            }
+            str = scan.nextLine();
+        }
+        System.out.println("Завершение работы");
+        if (app1.isVisible()) {
+            app1.setVisible(false);
+            app1.dispose();
+        }
+        if (app2.isVisible()) {
+            app2.setVisible(false);
+            app2.dispose();
+        }
     }
 }
-

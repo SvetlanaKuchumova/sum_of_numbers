8f283c8568242997eddb5769432221ea1f786a9d
svetlanaand <svetikpodv@yandex.ru>
2019-07-06 14:01:28 +0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введи свое имя");
+        String s1 = scan.nextLine();
+        System.out.println("Привет, " + s1 + "! Эта программа считает сумму двух целых чисел");
+        System.out.print("Введи первое число: ");
+        int number1 = scan.nextInt();
+        System.out.print("Введи второе число: ");
+        int number2 = scan.nextInt();
+        int sum = number1+number2;
+        System.out.println ("Сумма чисел равна " + sum);
+    }
+}

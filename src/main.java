32f91c2941cde131065be53dcd809fe0c8cd3841
svetlanaand <svetikpodv@yandex.ru>
2019-07-06 21:27:42 +0500

    1

--- src/main.java
+++ src/main.java
@@ -1,10 +1,16 @@
-import java.util.Scanner;
+import javax.swing.*;
+//import java.util.Scanner;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class main {
 
     public static void main(String[] args)
     {
-        Scanner scan = new Scanner(System.in);
+       // Simple
+
+     //   frame.setVisible(true);
+        /*Scanner scan = new Scanner(System.in);
         //System.out.println("Введи свое имя");
         //String s1 = scan.nextLine();
         //System.out.println("Привет, " + s1 + "! Эта программа считает сумму двух целых чисел");
@@ -13,6 +19,7 @@ public class main {
         System.out.print("Введи второе число: ");
         int number2 = scan.nextInt();
         int sum = number1+number2;
-        System.out.println ("Сумма чисел равна " + sum);
+        System.out.println ("Сумма чисел равна " + sum);*/
     }
 }
+
